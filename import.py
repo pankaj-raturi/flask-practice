8436a9d77f6fffdfa657810698150031a5e6931f
pankaj-tripat <pankaj@tripatinfoways.com>
2020-08-02 23:24:36 +0530

    Practice imports in Python

---
+++ import.py
@@ -0,0 +1,14 @@
+##!/usr/bin/env python
+
+# from imports.functions import *
+# from imports.functions import divide
+# from imports.functions import remainder
+import imports.functions
+
+
+
+# print(divide(8,2))
+print(imports.functions.divide(8,2))
+
+# print(remainder(10,3))
+print(imports.functions.remainder(10,3))
---
+++ imports/functions.py
@@ -0,0 +1,7 @@
+##!/usr/bin/env python3
+
+def divide(num1, num2):
+    return num1 / num2
+
+def remainder(num1, num2):
+    return num1 % num2
